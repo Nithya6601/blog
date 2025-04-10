2a95f04f6629805c8fb6a3eea577e350b10b9b70
Nithya6601 <154578084+Nithya6601@users.noreply.github.com>
2025-04-10 14:33:00 +0530

    Updated blog.service.ts

--- frontend-blog/src/app/services/blog.service.ts
+++ frontend-blog/src/app/services/blog.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BlogService {
-  private apiUrl = 'http://localhost:8000/api/blogs/';
+  private apiUrl = `${environment.apiUrl}blogs/`;
 
   constructor(private http: HttpClient) { }
 
